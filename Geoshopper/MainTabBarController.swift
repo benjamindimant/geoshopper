8abd72410a821341ce80eedfd42e0f957d915dcc
benjamindimant <benjamin@dimant.com>
2019-06-28 21:59:12 +0100

    Initial commit

---
+++ Geoshopper/MainTabBarController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.orange
+    }
+    
+}
---
+++ Geoshopper/SecondaryViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class SecondaryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+    
+}
